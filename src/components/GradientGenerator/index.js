a4c59e8b31d7c3e7738d4217e9c803367cd2f416
yaswanth <root@25c5fe54b6f6>
2021-11-28 05:46:17 +0000

    gradientGenerator

---
+++ src/components/GradientGenerator/index.js
@@ -0,0 +1,7 @@
+const gradientDirectionsList = [
+  {directionId: 'TOP', value: 'top', displayText: 'Top'},
+  {directionId: 'BOTTOM', value: 'bottom', displayText: 'Bottom'},
+  {directionId: 'RIGHT', value: 'right', displayText: 'Right'},
+  {directionId: 'LEFT', value: 'left', displayText: 'Left'},
+]
+// Write your code here
